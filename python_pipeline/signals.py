137e07ee928cfeb52f64b7c973962d0d156c2a34
Thanh Ly <luuthu357@gmail.com>
2026-05-07 09:52:57 -0700

    Add function to find reversal signals in prices

--- python_pipeline/signals.py
+++ python_pipeline/signals.py
@@ -1,1 +1,20 @@
+def find_reversal_signals(prices):
+    signals = []
 
+    for i in range(2, len(prices)):
+        two_days_ago = prices[i - 2]
+        yesterday = prices[i - 1]
+        today = prices[i]
+
+        two_days_ago_red = two_days_ago["close"] < two_days_ago["open"]
+        yesterday_red = yesterday["close"] < yesterday["open"]
+        today_green = today["close"] > today["open"]
+
+        if two_days_ago_red and yesterday_red and today_green:
+            signals.append({
+                "date": today["date"],
+                "signal": "possible_reversal",
+                "close": today["close"],
+            })
+
+    return signals
